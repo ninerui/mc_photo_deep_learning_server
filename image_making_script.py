933d4ad0c9a8c2ab3bfc9672dd1cb1840a514fe3
ninerui <2464250520>
2019-07-12 10:46:25 +0800

    change: 标签文本错误, 日志尝试修复

--- image_making_script.py
+++ image_making_script.py
@@ -10,6 +10,25 @@ import requests
 import numpy as np
 from tensorflow import keras
 
+try:
+    # Capirca uses Google's abseil-py library, which uses a Google-specific
+    # wrapper for logging. That wrapper will write a warning to sys.stderr if
+    # the Google command-line flags library has not been initialized.
+    #
+    # https://github.com/abseil/abseil-py/blob/pypi-v0.7.1/absl/logging/__init__.py#L819-L825
+    #
+    # This is not right behavior for Python code that is invoked outside of a
+    # Google-authored main program. Use knowledge of abseil-py to disable that
+    # warning; ignore and continue if something goes wrong.
+    import absl.logging
+
+    # https://github.com/abseil/abseil-py/issues/99
+    logging.root.removeHandler(absl.logging._absl_handler)
+    # https://github.com/abseil/abseil-py/issues/102
+    absl.logging._warn_preinit_stderr = False
+except Exception:
+    pass
+
 import conf
 from utils import connects, util
 from dl_module import image_making_interface, image_quality_assessment_interface
