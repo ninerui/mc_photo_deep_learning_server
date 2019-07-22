680e5df6251207bc079725ec51e9e05176603e8e
ninerui <2464250520>
2019-07-22 17:37:50 +0800

    change: 身份证模型名更改

--- dl_module/zhouwen_image_card_classify_interface.py
+++ dl_module/zhouwen_image_card_classify_interface.py
@@ -11,15 +11,16 @@ class IDCardClassify:
         graph_def.ParseFromString(f.read())
         self.sess.graph.as_default()
         tf.import_graph_def(graph_def, name='')
-        self.input = self.sess.graph.get_tensor_by_name('input_1_1:0')
-        self.softmax_tensor = self.sess.graph.get_tensor_by_name('dense_1_1/Softmax:0')
+        self.input = self.sess.graph.get_tensor_by_name('id_card_classify/input_1:0')
+        self.softmax_tensor = self.sess.graph.get_tensor_by_name('id_card_classify/dense_1/Softmax:0')
 
     def get_res(self, img):
-        img = np.expand_dims(np.expand_dims(img, axis=2), axis=0) / 255.0
-        pred = self.sess.run(self.softmax_tensor, {self.input: img})[0]
-        label = pred.index(max(pred))
-        confidence = max(pred)
-        if label == 0 and confidence > 0.95:
-            return 1
-        else:
-            return 0
+        with self.sess.graph.as_default():
+            img = np.expand_dims(np.expand_dims(img, axis=2), axis=0) / 255.0
+            pred = self.sess.run(self.softmax_tensor, {self.input: img})[0]
+            label = pred.index(max(pred))
+            confidence = max(pred)
+            if label == 0 and confidence > 0.95:
+                return 1
+            else:
+                return 0
