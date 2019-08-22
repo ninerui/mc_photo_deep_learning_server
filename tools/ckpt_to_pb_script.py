ef471e9dfed8a9b1c24eb7e4b1658c9873f3a02a
ninerui <2464250520>
2019-08-22 20:06:12 +0800

    change:

--- tools/ckpt_to_pb_script.py
+++ tools/ckpt_to_pb_script.py
@@ -10,9 +10,9 @@ def main():
     with tf.Graph().as_default():
         with tf.Session() as sess:
             saver = tf.train.import_meta_graph(
-                '../models/image_classification_models/oidv2-resnet_v1_101.ckpt/oidv2-resnet_v1_101.ckpt.meta')
+                './models/image_classification_models/oidv2-resnet_v1_101.ckpt/oidv2-resnet_v1_101.ckpt.meta')
             saver.restore(
-                sess, '../models/image_classification_models/oidv2-resnet_v1_101.ckpt')
+                sess, './models/image_classification_models/oidv2-resnet_v1_101.ckpt/oidv2-resnet_v1_101.ckpt')
 
             # Retrieve the protobuf graph definition and fix the batch norm nodes
             input_graph_def = sess.graph.as_graph_def()
@@ -22,7 +22,7 @@ def main():
                 sess, input_graph_def, ['input_values', "multi_predictions"])
 
         # Serialize and dump the output graph to the filesystem
-        with tf.gfile.GFile('../models/image_classification_models/oidv2-resnet_v1_101.pb', 'wb') as f:
+        with tf.gfile.GFile('./models/image_classification_models/oidv2-resnet_v1_101.pb', 'wb') as f:
             f.write(output_graph_def.SerializeToString())
 
 
