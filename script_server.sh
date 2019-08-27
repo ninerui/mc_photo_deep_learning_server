12c6283e3a5ab27d42cbe77751760114ab42f674
ninerui <2464250520>
2019-08-27 11:29:15 +0800

    add: 守护进程脚本

---
+++ script_server.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+conda activate py3
+cd /data/mc_photo_deep_learning_server/
+python manage_script.py -c $1
+
+# crontab
+# 3 * * * * /data/mc_photo_deep_learning_server/script_server.sh daemon
+
