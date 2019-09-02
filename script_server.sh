f4cc2fb2eb95d1e4370bd94587b16423768b2007
ninerui <2464250520>
2019-09-02 09:53:10 +0800

    change: 重启服务出错

--- script_server.sh
+++ script_server.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
-conda activate py3
 cd /data/mc_photo_deep_learning_server/
-python manage_script.py -c $1
+/root/miniconda3/envs/py3/bin/python /data/mc_photo_deep_learning_server/manage_script.py -c $1
+
+
+
+# python manage_script.py -c
 
 # crontab
 # 3 * * * * /data/mc_photo_deep_learning_server/script_server.sh daemon
